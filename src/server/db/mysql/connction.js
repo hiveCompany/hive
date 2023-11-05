981795fc9bfdb6d3d3e282408c43b6869e55b34e
yair-hive <yairman2429@gmail.com>
2023-11-05 19:32:16 +0200

    secend commet

---
+++ src/server/db/mysql/connction.js
@@ -0,0 +1,12 @@
+import mysql2 from 'mysql2';
+
+const con = mysql2.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '24290678',
+  database: 'hive',
+  multipleStatements: true,
+  port: 3306,
+});
+
+export default con;
