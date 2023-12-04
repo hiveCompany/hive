a1e981b6a9fe1ebbd13d983b71b08b2113b514a0
yair-hive <yairman2429@gmail.com>
2023-12-04 15:36:34 +0200

    fixing the connection problame

--- src/server/uams/test.js
+++ src/server/uams/test.js
@@ -1,11 +1,23 @@
-import uams from "./uams.js";
+// import uams from "./uams.js";
 
-const mSys = new uams();
+// const mSys = new uams();
 
-mSys.addOnStart(() => true);
-mSys.addOnStop(() => console.log("stop"));
-var newAction = mSys.createAction(function () {
-  console.log("action");
-});
+// mSys.addOnStart(() => true);
+// mSys.addOnStop(() => console.log("stop"));
+// var newAction = mSys.createAction(function () {
+//   console.log("action");
+// });
 
-newAction();
+// newAction();
+const handleDisconnect = function () {
+  console.log("hello");
+};
+setTimeout(handleDisconnect, 2000);
+const handleDisconnect1 = function () {
+  console.log("yair");
+};
+setTimeout(handleDisconnect1, 2000);
+const handleDisconnect2 = function () {
+  console.log("hello");
+};
+setTimeout(handleDisconnect2, 2000);
